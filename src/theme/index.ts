bc7f0e5a24c02423fcaa83c3be25a2339bc45ca7
poetrainy <manamisato2001@gmail.com>
2024-02-04 15:42:28 +0900

    fix: 不必要なスタイル設定を削除

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const colors = {
-};
 const styles = {
   global: {
     body: {
@@ -14,37 +12,16 @@ const styles = {
       li: {
         listStyleType: "none",
       },
-      pre: {
-        fontFamily: "body",
-        whiteSpace: "pre-wrap",
-      },
-    },
-    "::selection": {
-      background: "tomato",
-    },
-    "::-moz-selection": {
-      background: "tomato",
     },
   },
 };
 const fonts = {
   body: "'Hiragino Kaku Gothic ProN', 'Noto Sans JP', sans-serif",
 };
-const textStyles = {
-  bodyWidth: {
-    width: {
-      base: "90vw",
-      sm: "80vw",
-    },
-    mx: "auto",
-  },
-};
 
 const theme = extendTheme({
   styles,
-  colors,
   fonts,
-  textStyles,
 });
 
 export default theme;
